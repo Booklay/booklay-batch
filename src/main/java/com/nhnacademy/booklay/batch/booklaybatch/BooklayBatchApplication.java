2e37e8301cf037dabeef4d3033ebc644151fb3f4
yangrystal <tmddk512@naver.com>
2023-02-01 22:06:05 +0900

    init commit

---
+++ src/main/java/com/nhnacademy/booklay/batch/booklaybatch/BooklayBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.booklay.batch.booklaybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklayBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklayBatchApplication.class, args);
+    }
+
+}
